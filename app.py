d5b79800678c4f81a6bb0b81d7264b775a5bc87a
Roshan Sharma <reprogram46@gmail.com>
2019-10-19 16:34:25 +0530

    Initialize project structure

---
+++ app.py
@@ -0,0 +1,1 @@
+from blogstate import app
---
+++ blogstate/routes.py
@@ -0,0 +1,11 @@
+from blogstate import app
+
+
+@app.route('/')
+def home():
+    return "ooo"
+
+
+@app.route('/greet/<user>')
+def greet(user):
+    return f"hello, {user}"
