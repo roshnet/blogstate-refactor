a3ee22a1b83b61ae76194269085a76bcccce8b6f
Roshan Sharma <reprogram46@gmail.com>
2019-10-20 09:19:53 +0000

    feat: Add templates for login-signup routes
    Render these templates from the auth/ directory.

--- blogstate/routes/auth.py
+++ blogstate/routes/auth.py
@@ -5,10 +5,10 @@ from flask import render_template
 @app.route('/signin')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return f"Log In"
+    return render_template("auth/login.html")
 
 
 @app.route('/join')
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return f"Sign Up"
+    return render_template("auth/signup.html")
