5e4136621f4116777e3cc0cf38f1dd70c6de1eaa
Roshan Sharma <reprogram46@gmail.com>
2019-11-12 01:50:58 +0530

    fix: Pass sesison as user variable in posts template

--- blogstate/routes/general/blog.py
+++ blogstate/routes/general/blog.py
@@ -1,14 +1,18 @@
 from blogstate import app
 from blogstate.api import SecureAgent
-from flask import render_template
+from flask import (
+    render_template,
+    session
+)
 
 agent = SecureAgent()
 
 
-@app.route('/<username>')
+@app.route('/<username>/')
 def blog(username):
     """Fetch and display publically visible blog posts by an author"""
     posts = agent.fetch_posts_by_author(username)
     return render_template("posts.html",
+                           user=session,
                            posts=posts,
                            author=username)
