e1e3e8994025392abc047acbfd2bec8af1004ab3
Roshan Sharma <reprogram46@gmail.com>
2019-11-16 17:01:24 +0530

    home: Render dashboard when logged in

--- blogstate/routes/general/home.py
+++ blogstate/routes/general/home.py
@@ -1,7 +1,15 @@
 from blogstate import app
-from flask import render_template
+from flask import (
+    render_template,
+    redirect, session,
+    url_for
+)
 
 
 @app.route('/')
 def home():
+    if 'username' in session.keys():
+        if session['logged_in'] and session['username']:
+            return redirect(url_for('dashboard',
+                            username=session['username']))
     return render_template("home.html")
