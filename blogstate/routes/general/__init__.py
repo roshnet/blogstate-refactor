daf09d9f057a25cc5dcc00c6aeb839444f3bbf48
Roshan Sharma <reprogram46@gmail.com>
2019-12-25 13:51:41 +0530

    feat: Create post in Markdown; render markdown to view post

--- blogstate/routes/general/__init__.py
+++ blogstate/routes/general/__init__.py
@@ -1,7 +1,3 @@
-"""
-Route definitions for public routes.
-"""
-
 import blogstate.routes.general.home        # noqa
 import blogstate.routes.general.blog        # noqa
 import blogstate.routes.general.viewpost    # noqa
--- blogstate/routes/general/viewpost.py
+++ blogstate/routes/general/viewpost.py
@@ -1,6 +1,7 @@
 from blogstate import app
 from blogstate.api import SecureAgent
 from flask import render_template, session
+from markdown import markdown
 
 agent = SecureAgent()
 
@@ -8,6 +9,7 @@ agent = SecureAgent()
 @app.route('/<username>/post/<post_url>/')
 def get_post(username, post_url):
     post = agent.fetch_post_by_id(username, post_url)
+    post['body'] = markdown(post['body'])
     return render_template("view.html",
                            user=session,
                            post=post)
